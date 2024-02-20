5dfb7ea643c48c816952c4660227093971a5330a
pipeaalzamora <pipeaalzamora@gmail.com>
2024-02-20 14:42:35 -0300

    Se agrego analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <SpeedInsights />
+        <Analytics />
         <Footer />
       </body>
     </html>
